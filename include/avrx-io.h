4439affae0a5169d70e03d9e786c2a61188226c9
Neil Johnson <neil.johnson71@gmail.com>
2023-04-25 23:31:00 +0100

    Removed redundant include files

--- include/avrx-io.h
+++
@@ -1,4 +0,0 @@
-// 20021123 - Changed <io.h> to <avr/io.h> for 3.3 compiler
-
-#include <avr/io.h>
-
--- include/avrx-signal.h
+++
@@ -1,4 +0,0 @@
-// 20021123 - changed <sig-avr.h> to <avr/signal>  for 3.3 compiler
-
-#include <avr/interrupt.h>
-
